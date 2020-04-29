aa8ee729d9e3923f836b093a0a84c28be7404e09
Claire Kodia <claire.kodia@gmail.com>
2020-04-29 23:19:22 +0200

    Modal is structured

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,29 +1,27 @@
-import React from "react"
+import React, { useRef } from "react"
 import { NavLink } from "react-router-dom"
 
 import Modal from './Modal'
 import  "./LandingPage.css"
 
 
-
 const LandingPage = () => {
+    const modalRef = useRef();
 
-    const openModal = () => {
-        console.log("open Modal")
-    }
     return (
         <div className="landingPage">
             <div className="landing-text">
                 <p className="memory-travel">Memory Travel Game</p>
-                <NavLink activeClassName="active" exact to='/mainpage'>
+                {/* <NavLink activeClassName="active" exact to='/mainpage'>
                 <button className="button-enter" >Enter</button>
-                </NavLink>
-                <button onClick={openModal}>Open Modal</button>
-                <Modal>
-                    <h1>Modal Header</h1>
+                </NavLink> */}
+                <button className="button-enter" onClick={() => modalRef.current.openModal()}>New game</button>
+                <Modal ref={modalRef} >
+                    <h1>New Game</h1>
                     <p>
-                        Lorem im
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                     </p>
+                    <button onClick={() => modalRef.current.closeModal()}>Close</button>
                 </Modal>
             </div>
         </div>
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState, forwardRef, useImperativeHandle } from 'react';
+import ReactDOM from 'react-dom'
+
 
 import './Modal.css'
 
-const Modal = (props) => {
+const Modal = forwardRef((props, ref) => {
+    const [display, setDisplay] = useState(false)
+    
+    useImperativeHandle(ref, () => {
+        return {
+            openModal: () => open(),
+            closeModal: () => close(),
+        }
+    })
+
+    const open = () => setDisplay(true)
+    const close = () => setDisplay(false)
 
     
-    return (
-        <div className={"modal-wrapper"}>
-            <div className={"modal-backdrop"} />
-            <div className={"modal-box"}>
-                {props.children}
-            </div>
-           
-        </div>
-    )
-}
+    if (display) {
+        return ReactDOM.createPortal (
+            <div className={"modal-wrapper"}>
+                <div onClick={close}  className={"modal-backdrop"} />
+                <div className={"modal-box"}>
+                    {props.children}
+                </div>
+            </div>, document.getElementById("modal-root")
+        )
+    }
+
+    return null
+})
 
 export default Modal
