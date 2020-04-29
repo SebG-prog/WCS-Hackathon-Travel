01ebd18aa25b08227592fe95adb8c62ad82144a6
Marjolaine Baratte <marjolaine.baratte@gmail.com>
2020-04-29 16:25:25 +0200

    API Pixabay

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import APIPixabay from './components/APIPixabay'
 
 function App() {
   return (
     <div className="App">
+      <APIPixabay />
     </div>
   );
 }
