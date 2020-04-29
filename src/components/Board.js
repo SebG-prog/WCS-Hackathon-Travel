f33ac348156692d834be1f389910330272104f0f
SebG-prog <sebastien-gruet@orange.fr>
2020-04-29 14:51:38 +0200

    Initiating the React project for the Hackathon

---
+++ src/components/Board.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Board = () => {
+
+    return (
+        <div>Board</div>
+    )
+}
+
+export default Board
---
+++ src/components/Deck.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Deck = () => {
+
+    return (
+        <div>Deck</div>
+    )
+}
+
+export default Deck
