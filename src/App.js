a8711321f9bd43a54f2350e8e3a2432be1ce2bdb
Claire Kodia <claire.kodia@gmail.com>
2020-04-29 15:48:54 +0200

    Structured the file app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import GameSession from './components/GameSession'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <GameSession/>
     </div>
   );
 }
