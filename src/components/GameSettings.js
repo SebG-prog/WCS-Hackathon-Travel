4784c6299b71cf64e24dc11aadf7b980e69fd07d
Lola <lola.donval@gmail.com>
2020-04-30 02:31:16 +0200

    link api request to form

--- src/components/GameSettings.js
+++ src/components/GameSettings.js
@@ -1,45 +1,69 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from "react-router-dom"
 
 import Autocomplete from "./Autocomplete";
+import axios from 'axios';
+
+import './GameSettings.css'
 
 const GameSettings = () => {
-  const [input, setInput] = useState({})
+  const [dataFilter, setDataFilter] = useState([])
+  const [getCat, setGetCat] = useState('')
+  const [isLoaded, setIsLoaded] = useState(false)
 
-  const handleInputChange = (e) => setInput({
-    ...input,
-    [e.currentTarget.name]: e.currentTarget.value
-  })
-  
+  const categories = [
+    { type: 'food', img: '/img/food.jpg' },
+    { type: 'nature', img: '/img/nature.jpg' },
+    { type: 'places', img: '/img/places.jpg' },
+    { type: 'travel', img: '/img/travel.jpg' },
+    { type: 'beach', img: '/img/beach.jpg' },
+    { type: 'mountain', img: '/img/moutain.jpg' }
+  ]
+
+  const fetchDataFilter = async () => {
+    const resultFilter = await axios('https://cors-anywhere.herokuapp.com/http://countryapi.gear.host/v1/Country/getCountries?')
+    setDataFilter(resultFilter.data.Response.map(res => res.Name))
+    setIsLoaded(!isLoaded)
+  }
+
+  useEffect(() => {
+    fetchDataFilter()
+  }, [])
+
   return (
-    <form>
+    <div>
+      <form>
         <div>
-            <label>Which country do you want to travel to?</label>
-            {/* <input type="text" name="country" onChange={handleInputChange} /> */}
-            <Autocomplete
-                suggestions={[
-                    "Alligator",
-                    "Bask",
-                    "Crocodilian",
-                    "Death Roll",
-                    "Eggs",
-                    "Jaws",
-                    "Reptile",
-                    "Solitary",
-                    "Tail",
-                    "Wetlands"
-                ]}
-            />
+          <label>Which country do you want to travel to?</label>
+          <Autocomplete
+            suggestions={dataFilter}
+          />
         </div>
-        <div>
-            <label>What attracts you most?</label>
-            <input type="text" name="category" onChange={handleInputChange} />
+        <div className="cat-container" >
+          {
+            categories.map((cat, index) =>
+              <div
+                onClick={() => setGetCat(cat.type)}
+                className="cat"
+                key={index}
+                type={cat.type}>
+                <p className="cat-type">
+                  {cat.type}
+                </p>
+                <img
+                  className="cat-img"
+                  src={cat.img}
+                  alt={cat.type}
+                />
+              </div>
+            )
+          }
         </div>
         <NavLink activeClassName="active" exact to='/mainpage'>
-            <button>Start game</button>    
+          <button>Start game</button>
         </NavLink>
-      
-    </form>
+      </form>
+    </div>
   )
 }
 
