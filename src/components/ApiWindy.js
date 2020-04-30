e12f6ccabad9af74d6e2a08bfd41b816b51a22f7
SebG-prog <sebastien-gruet@orange.fr>
2020-04-30 11:56:16 +0200

    Displaying list of webcams from specific country

--- src/components/ApiWindy.js
+++ src/components/ApiWindy.js
@@ -1,39 +1,23 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
 
+
 const API_KEY = "v86LqZILmLPm1rCHTj4eDCcDGKc3Fveq";
-const countryId = "IT"
+const countryId = "FR"
 const category = "beach"
 const orderby = "popularity"
 const limit = 10
 
-//Timelapse in the player key
-
-//Actullement récupère un tableau de 10 webcams avec toutes les infos nécessaire dans data
-
 function ApiWindy() {
     const [data, setData] = useState([]);
-    // const [data2, setData2] = useState([]); // A remettre pour voir la liste des pays dispo (en dessous)
     const [isLoaded, setIsLoaded] = useState(false);
 
     const fetchData = async () => {
         setIsLoaded(false);
-
-        //Pour voir la liste des pays dispo
-
-        // const result2 = await axios(`https://api.windy.com/api/webcams/v2/list?show=countries`,
-        //     {
-        //         params: {
-        //             key: "v86LqZILmLPm1rCHTj4eDCcDGKc3Fveq"
-        //         }
-        //     })
-        //     setData2(console.log(result2.data.result.countries) || result2.data);
-
-
         const result = await axios(`https://api.windy.com/api/webcams/v2/list/country=${countryId}/category=${category}/orderby=${orderby}/limit=${limit}?show=webcams:location,image,player`,
             {
                 params: {
-                    key: "v86LqZILmLPm1rCHTj4eDCcDGKc3Fveq"
+                    key: API_KEY
                 }
             })
         setData(console.log(result.data.result.webcams) || result.data.result.webcams);
@@ -43,14 +27,30 @@ function ApiWindy() {
     useEffect(() => {
         fetchData();
     }, []);
-
+    console.log("test", data)
     return (
         <div>
             {isLoaded ?
-                <div>{data.map(
-                    image => <img src={image.image.daylight.preview} /> 
-                )}
-                </div>
+                (data.length !== 0 ?
+                    <div>{data.map(
+                        (image, i) =>
+                            <div key={i}>
+                                <div id="webcamGlobal">
+                                    <div id="webcamInfo">
+                                        <p>City: {image.location.city}</p>
+                                        <p>Region: {`${image.location.region} ( ${image.location.country} ) `}</p>
+                                        <p>Continent: {image.location.continent}</p>
+                                        <div>
+                                            <iframe title={image.location.city} width="560" height="315" src={image.player.day.embed} frameBorder="0" allowFullScreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    )}
+                    </div>
+                    :
+                    <h1>Sorry, but there is no footage from this location :/</h1>
+                )
                 : "loading..."}
         </div>
     )
