e8f4d0b8273ea2ac640f9e6964d9bcdf88829771
SebG-prog <sebastien-gruet@orange.fr>
2020-04-30 05:40:07 +0200

    Preparing third API call

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import ApiFilter from './components/ApiFilter'
 import { Route, Switch } from "react-router-dom";
 import LandingPage from './components/LandingPage';
 import MainPage from './components/MainPage';
@@ -11,8 +12,6 @@ import ApiWindy from './components/ApiWindy';
 function App() {
   return (
     <div className="App">
-      <ApiWindy />
-      <ApiFilter />
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route path="/mainpage" component={MainPage} />
--- src/components/ApiWindy.js
+++ src/components/ApiWindy.js
@@ -1,21 +1,35 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import axios from "axios"
 
-const API_KEY = "zmF43eOubhCOwkcrfCuqkeXfMebYU0pH";
+const API_KEY = "v86LqZILmLPm1rCHTj4eDCcDGKc3Fveq";
+const countryId = "CA"
+const category = "island"
+const orderby = "popularity"
+const limit = 10
 
 function ApiWindy() {
     const [data, setData] = useState([]);
+    const [data2, setData2] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false);
 
     const fetchData = async () => {
         setIsLoaded(false);
-        const result = await axios("https://api.windy.com/api/webcams/v2/list/country=IT/category=beach/orderby=popularity/limit=20", {
-            params: {
-                key: API_KEY,
-                show: `webcams:location,image`
-            }
-        })
-        setData(result.data);
+        const result2 = await axios(`https://api.windy.com/api/webcams/v2/list?show=countries`,
+            {
+                params: {
+                    key: "v86LqZILmLPm1rCHTj4eDCcDGKc3Fveq"
+                }
+            })
+            setData2(console.log(result2.data.result.countries) || result2.data);
+
+
+        const result = await axios(`https://api.windy.com/api/webcams/v2/list/country=${countryId}/category=${category}/orderby=${orderby}/limit=${limit}?show=webcams:location,image,player`,
+            {
+                params: {
+                    key: "v86LqZILmLPm1rCHTj4eDCcDGKc3Fveq"
+                }
+            })
+        setData(console.log(result.data.result.webcams) || result.data.result.webcams);
         setIsLoaded(true);
     };
 
@@ -25,7 +39,12 @@ function ApiWindy() {
 
     return (
         <div>
-            {isLoaded? <div>hi</div> : "loading..."}
+            {isLoaded ?
+                <div>{data.map(
+                    image => <img src={image.image.daylight.preview} />
+                )}
+                </div>
+                : "loading..."}
         </div>
     )
 
